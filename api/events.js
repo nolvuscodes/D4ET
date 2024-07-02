0a9d101c8363331b74e2e790f32fc6c6036e2887
Nolvus <hellomikko@outlook.com>
2024-07-02 07:14:21 +0000

    Updated

--- api/events.js
+++ api/events.js
@@ -6,9 +6,9 @@ module.exports = async (req, res) => {
         const { data } = await axios.get('https://helltides.com');
         const $ = cheerio.load(data);
 
-        const helltideTimer = $('.helltide-timer').text().trim();
-        const worldbossTimer = $('.worldboss-timer').text().trim();
-        const legionTimer = $('.legion-timer').text().trim();
+        const helltideTimer = $('.text-xl xl:text-[1.35rem] font-mono mb-2').text().trim();
+        const worldbossTimer = $('.text-xl xl:text-[1.35rem] text-center font-mono mb-2').text().trim();
+        const legionTimer = $('.text-xl xl:text-[1.35rem] text-center font-mono mb-2').text().trim();
 
         res.status(200).json({
             helltide: helltideTimer,
